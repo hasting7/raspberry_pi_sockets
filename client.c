f1f2625340e62155c0ea50a77d98f2b2397ee9cb
hasting7 <hasting7@purdue.edu>
2023-06-02 14:15:28 -0400

    inital commit

---
+++ client.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define PORT 5566
+
+int main() {
+	int mysocket;
+	struct sockaddr_in dest;
+	
+	mysocket = socket(AF_INET, SOCK_STREAM, 0);
+	
+	memset(&dest, 0, sizeof(dest));
+	dest.sin_family = AF_INET;
+	dest.sin_addr.s_addr = inet_addr("127.0.0.1");
+	dest.sin_port = htons(PORT);
+	
+	connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr_in));
+
+	printf("connected to server\n");
+
+	char buff[16] = { 0 };
+	recv(mysocket, buff, 16, 0);
+	
+	printf("message from server: %s\n",buff);
+	
+	close(mysocket);	
+
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define PORT 5566
+
+int main() {
+	struct sockaddr_in dest; // info about machine connecting to server
+	struct sockaddr_in serv; // info about server
+	int mysocket;
+
+	socklen_t socksize = sizeof(struct sockaddr_in);
+
+	memset(&serv, 0, sizeof(serv)); // zero out server struct
+	serv.sin_family = AF_INET; // set type of connection to TCP/IP
+	serv.sin_addr.s_addr = htonl(INADDR_ANY); // set our address to any interface
+	serv.sin_port = htons(PORT);
+
+	mysocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr)); // bind serv information to mysocket
+
+	// start listening and allow a queue of 'n' connections
+
+	for(;;) {
+		listen(mysocket, 1);
+		int consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
+		int *sock = malloc(sizeof(int *));
+		memcpy(sock, &consocket, sizeof(int));
+
+		printf("connected to client\ngonna send a message to client\n");
+
+		char buff[16] = "hello world";
+		send(*sock, buff, 16, 0);
+	
+
+		close(*sock);
+
+	}
+
+	close(mysocket);
+
+	return 0;
+}
