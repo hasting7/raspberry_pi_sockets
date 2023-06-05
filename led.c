0ebab726792ffbf13a3d988c036810224eebade0
hasting7 <hasting7@purdue.edu>
2023-06-05 18:42:06 -0400

    update

--- led.c
+++ led.c
@@ -3,9 +3,18 @@
 #include <bcm2835.h>
 
 
+int *root;
+root = 0x7E200000;
 
 int main() {
 
-	printf("%p\n",GPFSEL0);
+	printf("%p\n",root);
+
+	int mask = 512;
+	for (int i = 0; i < 32; i++) {
+		printf(((mask & *root) != 0) ? "1" : "0");
+	}
+	printf("\n");
+	
 	return 0;
 }
