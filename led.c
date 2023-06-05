83732343a23d1a3480ce7744dbd714c0e7268b4a
hasting7 <hasting7@purdue.edu>
2023-06-05 15:19:37 -0400

    update

--- led.c
+++ led.c
@@ -4,7 +4,11 @@
 
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 
+#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
+
 int main() {
 	INP_GPIO(7);
+	GPIO_CLR;
 	return 0;
 }
