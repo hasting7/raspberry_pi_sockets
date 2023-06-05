062ba552729a393127e122d12f153fa3b0b75b53
hasting7 <hasting7@purdue.edu>
2023-06-05 18:44:30 -0400

    update

--- led.c
+++ led.c
@@ -3,18 +3,21 @@
 #include <bcm2835.h>
 
 
-int *root;
-root = 0x7E200000;
+int *root = 0x7E200000;
 
 int main() {
 
 	printf("%p\n",root);
+	printf("%d\n",*root);
 
+#ifdef RTEST
 	int mask = 512;
 	for (int i = 0; i < 32; i++) {
 		printf(((mask & *root) != 0) ? "1" : "0");
+		mask = mask >> 1;
 	}
 	printf("\n");
 	
+#endif
 	return 0;
 }
