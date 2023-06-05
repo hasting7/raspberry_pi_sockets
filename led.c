66bbf1c491a6118de4a4944b8a333eadd07b7ca3
hasting7 <hasting7@purdue.edu>
2023-06-05 15:21:03 -0400

    update

--- led.c
+++ led.c
@@ -2,7 +2,12 @@
 #include <stdint.h>
 #include <bcm2835.h>
 
+#define gpio 0x7E200000
+
+// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
 
 #define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
 #define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
