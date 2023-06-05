58d4411a4d1d9e668fc1d5229107b70d6024f394
hasting7 <hasting7@purdue.edu>
2023-06-05 11:54:36 -0400

    update

--- led.c
+++ led.c
@@ -4,14 +4,20 @@
 
 #define PIN 26
 
+void generate_code(uint8_t *bits, int length) {
+
+}
+
 
 int main() {
 	wiringPiSetup();
 	pinMode(PIN, PWM_OUTPUT);
 
+	int length = 5;
 
-	uint8_t signal = 0x0;
-	
+	uint8_t signal[length * 24] = { 0 };
+
+
 	pwmWrite(PIN, signal);
 
 	return 0;
