bda55bc8b459e3fea0cdfac4f45b63e5be6bf430
hasting7 <hasting7@purdue.edu>
2023-07-20 20:44:17 -0400

    update

---
+++ pi_ready.c
@@ -0,0 +1,12 @@
+#include <wiringPi.h>
+
+#define ON_LIGHT 4
+
+int main() {
+	wiringPiSetup();
+	pinMode(ON_LIGHT, OUTPUT);
+
+	digitalWrite(ON_LIGHT,LOW);
+	digitalWrite(ON_LIGHT,HIGH);
+	return 0;
+}
