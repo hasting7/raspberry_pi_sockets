a6fe2f3695f8dee899aa6f284d79a388a26ae022
hasting7 <hasting7@purdue.edu>
2024-02-07 17:58:09 -0500

    update

--- initalize_server.sh
+++ initalize_server.sh
@@ -5,7 +5,10 @@
 cd $1
 
 if [[ $(cat SERVER_STATE | egrep -x '0') ]]; then
-	./web >> setup.out 2> error.out|| echo 1 > SERVER_STATE
+	./web > setup.out 2> error.out
+	if [ -z $? ]; then
+		echo 1 > SERVER_STATE
+	fi
 
 else 
 	echo server already running or error check error.out
