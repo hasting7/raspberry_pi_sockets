a2a3e9c4bc8f7b96cbe4fd857e9aeabf34193d1e
hasting7 <hasting7@purdue.edu>
2024-02-08 00:00:30 -0500

    update

--- server-logs/boot_script.sh
+++ server-logs/boot_script.sh
@@ -23,13 +23,17 @@ for file in "state.out" "setup.out"; do
 		echo "$file DNE" >> "$log_dir/error.out" 
 		exit 1
 	fi
+	echo "$log_dir/$file found" >> "$log_dir/setup.out" 
 done
 for file in "header" "main.html"; do
 	if [ ! -e "$web_dir/$file" ]; then
 		echo "$file DNE" >> "$log_dir/error.out" 
 		exit 1
 	fi
+	echo "$web_dir/$file found" >> "$log_dir/setup.out" 
 done
+echo "files validated" >> "$log_dir/setup.out"
 
+# cd "$log_dir"
 # ./initalize_server.sh .. || echo "server set from boot failed " >> error.out
 ./web "$web_dir/main.html" "$web_dir/header" > "$log_dir/setup.out" 2> "$log_dir/error.out"
